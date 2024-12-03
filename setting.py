7473968d37f5ac26f8a55bcf309ad73e62b27e6f
GYQQ_NFQ <hehaolin37@gmail.com>
2024-12-03 22:06:18 +0800

    保留qmsg防止send检查不到模块

--- setting.py
+++ setting.py
@@ -159,7 +159,7 @@ QYWX_KEY = ""
 # WECHAT_WARNING_PHONE = ""  # 报警人 将会在群内@此人, 支持列表，可指定多人
 # WECHAT_WARNING_ALL = False  # 是否提示所有人， 默认为False
 # QMSG报警
-# QMSG_WARNING_URL = ""  # qmsg机器人api
+QMSG_WARNING_URL = ""  # qmsg机器人api
 # QMSG_WARNING_QQ = ['']  # 指定要接收消息的QQ号或者QQ群。多个以英文逗号分割，例如：12345,12346,支持列表，可指定多人
 # QMSG_WARNING_BOT = ""  # 机器人的QQ号
 # # 时间间隔
